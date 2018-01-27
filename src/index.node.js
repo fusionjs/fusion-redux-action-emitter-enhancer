b062073677ab88e71d6e53e2bd7d4e207870292c
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 16:17:59 -0800

    Fix tree shaking

---
+++ src/index.node.js
@@ -0,0 +1,31 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint-env browser,node */
+import {createPlugin} from 'fusion-core';
+import {UniversalEventsToken} from 'fusion-plugin-universal-events';
+
+export default createPlugin({
+  deps: {
+    emitter: UniversalEventsToken,
+  },
+  provides({emitter}) {
+    if (__DEV__ && !emitter) {
+      throw new Error(`emitter is required, but was: ${emitter}`);
+    }
+
+    return createStore => (...args) => {
+      const store = createStore(...args);
+      return {
+        ...store,
+        dispatch: action => {
+          emitter.from(store.ctx).emit('redux-action-emitter:action', action);
+          return store.dispatch(action);
+        },
+      };
+    };
+  },
+});
